37176b42ff747acba4f9b7a4899832b2ae17bc70
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-10 18:49:23 +0100

    Added pokemon class

---
+++ js/PokemonClass.js
@@ -0,0 +1,11 @@
+export class Pokemon{
+    constructor({name, hp, attack, defense, speed, type, moves}){
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.type = type;
+        this.moves = moves;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-import { getPokemon } from "./api.js" 
+import {getPokemon, getMove} from "./api.js";
+import { Pokemon } from "./PokemonClass.js"
 
-// document.querySelector(".api-btn").addEventListener("click", () => {
-//     getPokemon();
-// })
+
+document.querySelector(".api-btn").addEventListener("click", async() => {
+    await startBattle();
+});
+
+const createPlayerStates = async() => {
+    const p1Pokemon = document.querySelector(".player-one-pokemon").value;
+    const p2Pokemon = document.querySelector(".player-two-pokemon").value;
+
+    const pokemonOne = await getPokemon(p1Pokemon);
+
+    for (let index = 0; index < 4; index++) {
+        getMove();
+    }
+
+
+    // await getPokemon(p2Pokemon);
+};
+
+const startBattle = async() => {
+    await createPlayerStates();
+    // getMove();
+};
